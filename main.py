61be775293f9a9968f7c5106aab084f28b9fe523
Lily <lilyc5459@gmail.com>
2014-12-13 17:21:07 -0500

    button class

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ BLACK = (   0,   0,   0)
 WHITE = ( 255, 255, 255)
 GREEN = (   0, 255,   0)
 RED   = ( 255,   0,   0)
+FONT  = "monospace"
 
 pygame.init()
-
 size = (600, 400)
 screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("3D Tic Tac Toe - Lily Chen - Jim Yu")
+pygame.display.set_caption("3D Tic Tac Toe - Lillian Chen & Jim Yu")
 
 clock = pygame.time.Clock()
 
@@ -24,6 +23,21 @@ ABOUT = 3
 
 state = BEGIN
 
+class Button:
+	mouseOn = False
+
+	def __init__(self, color, rect, content):
+		self.color = color
+		self.rect = pygame.Rect(rect)
+		self.content = content
+		self.rend = content_init.render(self.content, 1, BLACK)
+
+	def draw(self):
+		pygame.draw.rect(screen, self.color, self.rect, 2)
+		textpos = self.rend.get_rect()
+		textpos.center = self.rect.center
+		screen.blit(self.rend, textpos)
+
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -33,31 +47,37 @@ while running:
 
 	if state == BEGIN:
 		# start screen
-		myfont = pygame.font.SysFont("monospace", 40)
-		play = myfont.render("Play", 1, BLACK)
-		learn = myfont.render("Learn", 1, BLACK)
-		about = myfont.render("About", 1, BLACK)
-		pygame.draw.rect(screen, RED, [225, 50, 175, 90], 2)
-		pygame.draw.rect(screen, RED, [225, 150, 175, 90], 2)
-		pygame.draw.rect(screen, RED, [225, 250, 175, 90], 2)
-		screen.blit(play, (263,75))
-		screen.blit(learn, (250, 175))
-		screen.blit(about, (250, 275))
+		content_init = pygame.font.SysFont(FONT, 30)
+		menu = [Button(BLACK, [225, 50, 150, 80], "Play"),
+						Button(BLACK, [225, 150, 150, 80], "Learn"),
+						Button(BLACK, [225, 250, 150, 80], "About")]
+
+		pygame.event.pump()
+		for button in menu:
+			button.draw()
+			if button.rend.get_rect().collidepoint(pygame.mouse.get_pos()):
+				button.mouseOn = True
+			else:
+				button.mouseOn = False
+		pygame.display.flip()
 
 	if state == PLAY:
 		# draw the board
-		xbase1 = 25
+		xbase1 = 50
 		xbase2 = 225
-		xbase3 = 425
-		for y in [100, 150, 200]:
+		xbase3 = 400
+		for y in [125, 175, 225]:
 			for i in range(3):
 				add = i * 50
-				pygame.draw.rect(screen, GREEN, [xbase1 + add, y, 50, 50], 2)
-				if y == 150 and add == 50:
+				pygame.draw.rect(screen, BLACK, [xbase1 + add, y, 50, 50], 2)
+				if y == 175 and add == 50:
 					pygame.draw.rect(screen, BLACK, [xbase2 + add, y, 50, 50])
 				else:
-					pygame.draw.rect(screen, GREEN, [xbase2 + add, y, 50, 50], 2)
-				pygame.draw.rect(screen, GREEN, [xbase3 + add, y, 50, 50], 2)
+					pygame.draw.rect(screen, BLACK, [xbase2 + add, y, 50, 50], 2)
+				pygame.draw.rect(screen, BLACK, [xbase3 + add, y, 50, 50], 2)
+
+		# if event.type == pygame.MOUSEBUTTONDOWN:
+
 
 	if state == ABOUT:
 		pass
