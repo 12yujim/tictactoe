b58fa58253dc81b15e2b42b8f334bca27703d2da
Lily <lilyc5459@gmail.com>
2014-12-13 19:01:37 -0500

    gui play

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ class Button:
 		self.color = color
 		self.rect = pygame.Rect(rect)
 		self.content = content
-		self.rend = content_init.render(self.content, 1, BLACK)
 
 	def draw(self):
+		self.rend = content_init.render(self.content, 1, BLACK)
 		pygame.draw.rect(screen, self.color, self.rect, 2)
 		textpos = self.rend.get_rect()
 		textpos.center = self.rect.center
@@ -28,6 +28,19 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("3D Tic Tac Toe - Lillian Chen & Jim Yu")
 clock = pygame.time.Clock()
 running = True
+player = 1
+
+# draw the board
+squares = []
+for y in [125, 175, 225]:
+	for i in range(3):
+		add = i * 50
+		squares.append(Button(BLACK, [50 + add, y, 50, 50], ""))
+		if y == 175 and add == 50:
+			pass
+		else:
+			squares.append(Button(BLACK, [225 + add, y, 50, 50], ""))
+		squares.append(Button(BLACK, [400 + add, y, 50, 50], ""))
 
 # game states
 BEGIN = 0
@@ -67,20 +80,19 @@ while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
-		# draw the board
-		xbase1 = 50
-		xbase2 = 225
-		xbase3 = 400
-		for y in [125, 175, 225]:
-			for i in range(3):
-				add = i * 50
-				pygame.draw.rect(screen, BLACK, [xbase1 + add, y, 50, 50], 2)
-				if y == 175 and add == 50:
-					pygame.draw.rect(screen, BLACK, [xbase2 + add, y, 50, 50])
-				else:
-					pygame.draw.rect(screen, BLACK, [xbase2 + add, y, 50, 50], 2)
-				pygame.draw.rect(screen, BLACK, [xbase3 + add, y, 50, 50], 2)
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				for square in squares:
+					if square.rect.collidepoint(event.pos):
+						if player == 1:
+							square.content = "X"
+							player = 2
+						elif player == 2:
+							square.content = "O"
+							player = 1
 
+		for square in squares:
+			square.draw()
+		pygame.draw.rect(screen, BLACK, [275, 175, 50, 50])
 
 	if state == ABOUT:
 		pass
