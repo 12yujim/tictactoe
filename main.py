164b88f844ada4b7a2278364771871ccd88f2a45
Lily <lilyc5459@gmail.com>
2014-12-14 14:00:24 -0500

    chooses turn

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ BLACK = (   0,   0,   0)
 WHITE = ( 255, 255, 255)
 GREEN = (   0, 255,   0)
 RED   = ( 255,   0,   0)
-FONT  = "monospace"
+FONT  = "font/gameboy.ttf"
 
 class Button:
 	mouseOn = False
@@ -36,6 +36,7 @@ def initialize():
 	global gameboard
 	global winner
 	global player
+
 	# draw the board
 	squares = []
 	for y in range(3):
@@ -48,6 +49,7 @@ def initialize():
 			else:
 				squares.append(Button(BLACK, [225 + x_coord, y_coord, 50, 50], "", (x, y, 1)))
 			squares.append(Button(BLACK, [400 + x_coord, y_coord, 50, 50], "", (x, y, 2)))
+	
 	# initialize the gameboard
 	gameboard = board.Board()
 	winner = 0
@@ -55,10 +57,11 @@ def initialize():
 
 # game states
 BEGIN = 0
-PLAY = 1
-LEARN = 2
-ABOUT = 3
-END = 4
+CHOOSE = 1
+PLAY = 2
+LEARN = 3
+ABOUT = 4
+END = 5
 
 state = BEGIN
 
@@ -69,10 +72,10 @@ while running:
 	
 	# start screen
 	if state == BEGIN:
-		content_init = pygame.font.SysFont(FONT, 30)
-		menu = [Button(BLACK, [225, 50, 150, 80], "Play"),
-						Button(BLACK, [225, 150, 150, 80], "Learn"),
-						Button(BLACK, [225, 250, 150, 80], "About")]
+		content_init = pygame.font.Font(FONT, 15)
+		menu = [Button(BLACK, [225, 100, 150, 60], "Play"),
+						Button(BLACK, [225, 175, 150, 60], "Learn"),
+						Button(BLACK, [225, 250, 150, 60], "About")]
 
 		for button in menu:
 			button.draw()
@@ -84,13 +87,32 @@ while running:
 				for button in menu:
 					if button.rect.collidepoint(event.pos):
 						if button.content == "Play":
-							state = PLAY
+							state = CHOOSE
 						if button.content == "About":
 							state = ABOUT
 						else:
 							pass
 
+	if state == CHOOSE:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_y:
+					player = 1
+					state = PLAY
+				elif event.key == pygame.K_n:
+					player = 2
+					state = PLAY
+		prompt = pygame.font.Font(FONT, 15)
+		prompt_text = "Would you like to go first? y or n"
+		label = prompt.render(prompt_text, 1, BLACK)
+		label_pos = label.get_rect()
+		label_pos.center = screen.get_rect().center
+		screen.blit(label, label_pos)
+
 	if state == PLAY:
+		content_init = pygame.font.Font(None, 25)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
@@ -105,7 +127,7 @@ while running:
 								square.content = "X"
 								player = 2
 							except:
-								pass
+								state = BEGIN
 						elif player == 2:
 							try:
 								if gameboard.update(square.pos, 2):
@@ -114,11 +136,29 @@ while running:
 								square.content = "O"
 								player = 1
 							except:
-								pass
+								state = BEGIN
 
 		for square in squares:
 			square.draw()
 		pygame.draw.rect(screen, BLACK, [275, 175, 50, 50])
+		playfont = pygame.font.Font(FONT, 15)
+		if player == 1:
+			turn = "turn: you"
+		if player == 2:
+			turn = "turn: computer"
+		top = "top"
+		middle = "middle"
+		bottom = "bottom"
+		turn_rend = playfont.render(turn, 1, BLACK)
+		top_rend = playfont.render(top, 1, BLACK)
+		mid_rend = playfont.render(middle, 1, BLACK)
+		bot_rend = playfont.render(bottom, 1, BLACK)
+		mid_pos = mid_rend.get_rect()
+		mid_pos.centerx = screen.get_rect().centerx
+		screen.blit(turn_rend, (50, 75))
+		screen.blit(top_rend, (105, 300))
+		screen.blit(mid_rend, (mid_pos.left, 300))
+		screen.blit(bot_rend, (435, 300))
 
 	if state == ABOUT:
 		pass
@@ -131,14 +171,23 @@ while running:
 				if event.key == pygame.K_RETURN:
 					initialize()
 					state = BEGIN
-		winfont = pygame.font.SysFont(FONT, 50)
-		replayfont = pygame.font.SysFont(FONT, 30)
-		the_win = "Player %s wins!" % winner
+		winfont = pygame.font.Font(FONT, 20)
+		replayfont = pygame.font.Font(FONT, 10)
+		if winner == 1:
+			the_win = "You win!"
+		elif winner == 2:
+			the_win = "You lose!"
+		else:
+			the_win = "It's a tie!"
 		replay = "Press ENTER"
 		label = winfont.render(the_win, 1, BLACK)
 		label2 = replayfont.render(replay, 1, BLACK)
-		screen.blit(label, (100,150))
-		screen.blit(label2, (175, 250))
+		label_pos = label.get_rect()
+		label_pos.centerx = screen.get_rect().centerx
+		label2_pos = label2.get_rect()
+		label2_pos.centerx = screen.get_rect().centerx
+		screen.blit(label, (label_pos.left, 175))
+		screen.blit(label2, (label2_pos.left, 240))
 
 	pygame.display.flip()
 	clock.tick(60)
